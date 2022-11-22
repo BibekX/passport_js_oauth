68055b45e9b94b340386fa15db191cb11210d3dc
Bibek Rajbhandari <bibekxlmao@gmail.com>
2022-11-22 13:23:35 +0545

    added flash-msg

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //Requires modules
-// const https = require("https");
-// const fs = require("fs");
+const https = require("https");
+const fs = require("fs");
 const express = require("express");
 const { engine } = require("express-handlebars");
 const knexFile = require("./knexfile").development;
@@ -8,9 +8,10 @@ const knex = require("knex")(knexFile);
 const session = require("express-session");
 const passport = require("passport");
 const bcrypt = require("bcrypt");
+const flash = require("express-flash");
 const LocalStrategy = require("passport-local").Strategy;
-// const FacebookStrategy = require("passport-facebook").Strategy;
-// const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
+const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
 
 const port = 3000;
 require("dotenv").config();
@@ -68,7 +69,9 @@ passport.use(
       };
       const id = await knex("users").insert(newUser).returning("id"); //[{id: 4}]
       newUser.id = id[0].id; //id[0] => {id: 4}     id[0].id => 4
-      return done(null, newUser);
+      return done(null, newUser, {
+        message: "User already exists with this email",
+      });
     }
   )
 );
@@ -81,83 +84,85 @@ passport.use(
     async (email, password, done) => {
       const user = await knex("users").where({ email }).first();
       if (!user) {
-        return done(null, false);
+        return done(null, false, {
+          message: "User does not exist with this email",
+        });
       }
       const result = await bcrypt.compare(password, user.password);
       if (result) {
         return done(null, user);
       }
-      return done(null, false);
+      return done(null, false, { message: "Password incorrect" });
     }
   )
 );
 
 //Facebook Strategy
-// passport.use(
-//   "facebook",
-//   new FacebookStrategy(
-//     {
-//       clientID: process.env.FACEBOOK_APP_ID,
-//       clientSecret: process.env.FACEBOOK_SECRET,
-//       callbackURL: "https://localhost:3000/auth/facebook/callback",
-//       profileFields: ["id", "email", "name"],
-//     },
-//     async (accessToken, refreshToken, profile, done) => {
-//       try {
-//         const user = await knex("users")
-//           .where({ facebook_id: profile.id })
-//           .first();
-//         if (!user) {
-//           let newUser = {
-//             facebook_id: profile.id,
-//             email: profile._json.email,
-//           };
-//           const id = await knex("users").insert(newUser).returning("id");
-//           newUser.id = id[0].id;
-//           return done(null, newUser);
-//         } else {
-//           return done(null, user);
-//         }
-//       } catch (err) {
-//         console.log(err);
-//         return done(err, false);
-//       }
-//     }
-//   )
-// );
+passport.use(
+  "facebook",
+  new FacebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_APP_ID,
+      clientSecret: process.env.FACEBOOK_SECRET,
+      callbackURL: "https://localhost:3000/auth/facebook/callback",
+      profileFields: ["id", "email", "name"],
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const user = await knex("users")
+          .where({ facebook_id: profile.id })
+          .first();
+        if (!user) {
+          let newUser = {
+            facebook_id: profile.id,
+            email: profile._json.email,
+          };
+          const id = await knex("users").insert(newUser).returning("id");
+          newUser.id = id[0].id;
+          return done(null, newUser);
+        } else {
+          return done(null, user);
+        }
+      } catch (err) {
+        console.log(err);
+        return done(err, false);
+      }
+    }
+  )
+);
 
 //Google Strategy
-// passport.use(
-//   "google",
-//   new GoogleStrategy(
-//     {
-//       clientID: process.env.GOOGLE_ID,
-//       clientSecret: process.env.GOOGLE_SECRET,
-//       callbackURL: "https://localhost:3000/auth/google/callback",
-//     },
-//     async (accessToken, refreshToken, profile, done) => {
-//       try {
-//         const user = await knex("users")
-//           .where({ google_id: profile.id })
-//           .first();
-//         if (!user) {
-//           let newUser = {
-//             google_id: profile.id,
-//             email: profile._json.email,
-//           };
-//           const id = await knex("users").insert(newUser).returning("id");
-//           newUser.id = id[0].id;
-//           return done(null, newUser);
-//         } else {
-//           return done(null, user);
-//         }
-//       } catch (err) {
-//         console.log(err);
-//         return done(err, false);
-//       }
-//     }
-//   )
-// );
+passport.use(
+  "google",
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      callbackURL: "https://localhost:3000/auth/google/callback",
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const user = await knex("users")
+          .where({ google_id: profile.id })
+          .first();
+        if (!user) {
+          let newUser = {
+            google_id: profile.id,
+            email: profile._json.email,
+          };
+          const id = await knex("users").insert(newUser).returning("id");
+          newUser.id = id[0].id;
+          return done(null, newUser);
+        } else {
+          return done(null, user);
+        }
+      } catch (err) {
+        console.log(err);
+        return done(err, false);
+      }
+    }
+  )
+);
 
 //Serialize
 passport.serializeUser((user, done) => {
@@ -183,12 +188,14 @@ app.get("/", isLoggedIn, (req, res) => {
 app.get("/signup", notLoggedIn, (req, res) => {
   res.render("signup", {
     title: "Sign Up",
+    error: req.flash("error"),
   });
 });
 
 app.get("/login", notLoggedIn, (req, res) => {
   res.render("login", {
     title: "Login",
+    error: req.flash("error"),
   });
 });
 
@@ -219,43 +226,43 @@ app.post(
 );
 
 //Handle Facebook Login
-// app.get(
-//   "/auth/facebook",
-//   passport.authenticate("facebook", {
-//     scope: ["email", "public_profile"],
-//   })
-// );
+app.get(
+  "/auth/facebook",
+  passport.authenticate("facebook", {
+    scope: ["email", "public_profile"],
+  })
+);
 
-// app.get(
-//   "/auth/facebook/callback",
-//   passport.authenticate("facebook", {
-//     successRedirect: "/",
-//     failureRedirect: "/login",
-//   })
-// );
+app.get(
+  "/auth/facebook/callback",
+  passport.authenticate("facebook", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+  })
+);
 
 //Handle Google Login
-// app.get(
-//   "/auth/google",
-//   passport.authenticate("google", {
-//     scope: ["email", "profile"],
-//   })
-// );
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["email", "profile"],
+  })
+);
 
-// app.get(
-//   "/auth/google/callback",
-//   passport.authenticate("google", {
-//     successRedirect: "/",
-//     failureRedirect: "/login",
-//   })
-// );
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+  })
+);
 
-// const options = {
-//   cert: fs.readFileSync("./localhost.crt"),
-//   key: fs.readFileSync("./localhost.key"),
-// };
+const options = {
+  cert: fs.readFileSync("./localhost.crt"),
+  key: fs.readFileSync("./localhost.key"),
+};
 
-app.listen(port, () => console.log(`Listening to port ${port}`));
-// https
-//   .createServer(options, app)
-//   .listen(3000, () => console.log(`Listening to port ${port}`));
+// app.listen(port, () => console.log(`Listening to port ${port}`));
+https
+  .createServer(options, app)
+  .listen(3000, () => console.log(`Listening to port ${port}`));
